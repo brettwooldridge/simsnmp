4715fb932bc7c8cb93bc77a1d53d81273709f197
Brett Wooldridge <brett.wooldridge@gmail.com>
2019-09-18 18:40:07 +0900

    Add log4j2.yaml configuration.

--- loadtest-sim.sh
+++ loadtest-sim.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-/usr/bin/java -Djava.security.egd=file:/dev/./urandom -Dhandler=./load-agent.js -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:8887 -jar /root/sim/simsnmp-all.jar
+/usr/bin/java -Djava.security.egd=file:/dev/./urandom -Dhandler=./load-agent.js -Dlog4j.configurationFile=./log4j2.yaml -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:8887 -jar ./simsnmp-all.jar
